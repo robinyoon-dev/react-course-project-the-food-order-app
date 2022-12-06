6bda09432bcd1dc5beb42068c83e6a73c4732f58
Yoon Robin <robinyoonofficial@gmail.com>
2022-12-06 18:05:13 +0900

    add modal(not using portal)

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react"; 'react';
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+    return (
+        <Fragment>
+            <Backdrop />
+            <ModalOverlay>{props.children}</ModalOverlay>
+        </Fragment>
+    );
+};
+
+export default Modal;
