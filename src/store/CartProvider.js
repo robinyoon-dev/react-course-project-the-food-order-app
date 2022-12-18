1b4a5970c1a4178de3c39d37b25399356b4aa4f9
Yoon Robin <robinyoonofficial@gmail.com>
2022-12-18 09:01:14 +0900

    add a function how to deal with existingCartItem

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,11 +9,30 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem; 
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
   }
   return defaultCartState;
@@ -34,9 +53,9 @@ const CartProvider = (props) => {
 
   const removeItemFromCartHandler = (id) => {
     dispatchCartAction({
-        type: "REMOVE",
-        id: id,
-      });   
+      type: "REMOVE",
+      id: id,
+    });
   };
 
   const cartContext = {
